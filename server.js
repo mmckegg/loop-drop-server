add23a415e1e7a507e9d4597cb0fd388ccecf32a
Matt McKegg <matt@wetsand.co.nz>
2014-07-30 19:47:58 +1200

    basic sample file transfer between clients

--- server.js
+++ server.js
@@ -1,82 +1,38 @@
-var server = require('http').createServer()
+var FileTransfer = require('./lib/file-transfer')
+var Sockets = require('./lib/sockets')
 
-var WebSocketServer = require('ws').Server
-var websocket = require('websocket-stream')
-var wss = new WebSocketServer({server: server})
+var router = require('routes-router')()
+var cors = require('./lib/cors')
 
-var clients = []
-var clientLookup = {}
+var server = require('http').createServer(handleRequest)
+var fileTransfer = FileTransfer(__dirname + '/file-cache', sendFileRequest)
+var sockets = Sockets(server)
 
-var nextId = 1
-
-function broadcast(message){
-  for (var i=0;i<clients.length;i++){
-    var client = clients[i]
-    if (message.from !== client.clientId){ // prevent feedback
-      clients[i].write(JSON.stringify(message))
-    }
-  }
+function handleRequest(req, res){
+  cors(req, res, router)
 }
 
-function direct(message, to){
-  if (Array.isArray(to)){
-    for (var i=0;i<to.length;i++){
-      clientLookup[i]&&clientLookup[i].write(JSON.stringify(message))
-    }
-  } else {
-    clientLookup[to]&&clientLookup[to].write(JSON.stringify(message))
+router.addRoute('/files/:clientId/:fileName.wav', {
+  GET: function(req, res, opts){
+    var src = opts.params.fileName + '.wav'
+    fileTransfer.getFile(req, res, src, parseInt(opts.params.clientId))
+  },
+  POST: function(req, res, opts){
+    var src = opts.params.fileName + '.wav'
+    console.log('RECEIVING FILE:', src)
+    fileTransfer.postFile(req, res, src)
   }
-}
-
-wss.on('connection', function(ws) {
-  var socket = websocket(ws)
-  socket.clientId = nextId++
-  console.log('client connect', socket.clientId)
-
-  // let client know their ID
-  process.nextTick(function(){
-    direct({
-      from: 'server', 
-      clientId: socket.clientId
-    }, socket.clientId)
-  })
-
-  // listen for messages
-  socket.on('data', function(data){
-    var message = JSON.parse(data)
-    message.from = socket.clientId
-    if (message.to){
-      direct(message, message.to)
-    } else {
-      broadcast(message)
-    }
-  })
-
-  function cleanUp(){
-    var index = clients.indexOf(socket)
-    if (~index){
-      clients.splice(index, 1)
-      clientLookup[socket.clientId] = null
-      console.log('client disconnect', socket.clientId)
-
-      broadcast({
-        from: 'server',
-        clientDisconnect: socket.clientId
-      })
-    }
-  }
-
-  // handle disconnect
-  socket.on('close', cleanUp)
-
-  socket.on('error', function(err){
-    console.log(err)
-  })
-
-  // store the connection for later
-  clients.push(socket)
-  clientLookup[socket.clientId] = socket
 })
 
+function sendFileRequest(path, clientId){
+  var message = {
+    from: 'server',
+    requestFile: path,
+    to: clientId
+  }
+  console.log(message)
+  sockets.direct(message, message.to)
+}
+
 server.listen(7777)
-console.log('Listening on ws://localhost:7777')
+console.log('Listening on localhost:7777')
